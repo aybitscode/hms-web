df077f4438f6e5d82b14e4fc973db8af057d0008
Mohammed Sibgathullah <javatiddler@gmail.com>
2018-08-14 09:59:01 +0530

    Hoteldocuments created from previous Hotel model in order to match with the json provided by Basha

---
+++ src/app/theme/pages/default/_models/hoteldocuments.ts
@@ -0,0 +1,11 @@
+export class Hoteldocuments {
+    registrationNo: string;
+    buildingPermitNumber: string;
+    fireSafetyNumber: string;
+    policeLicenseNumber: string;
+    healthTradeLicenseNumber: string;
+    fssaiLicenseNumber: string;
+    gstNumber: string;
+    esiRegistrationNumber: string;
+    epfRegistrationNumber: string;
+}
